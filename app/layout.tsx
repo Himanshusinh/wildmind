6c078632052003b8ff8187e38386ea140912a38a
Himanshu Vaghela <himanshu@example.com>
2025-04-24 15:16:04 +0530

    logo added

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head />
+      <link rel="icon" href="/logomain.png" />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
